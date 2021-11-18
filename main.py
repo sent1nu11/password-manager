f9b22f6c3380f87d7a6a852ea60a51bd469e6b17
sent1nu11 <sent1nu11@gmail.com>
2021-11-17 23:49:50 -0700

    1

--- main.py
+++ main.py
@@ -26,10 +26,12 @@ password_label.grid(row=3, column=0)
 #Entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "roger@gmail.com")
 password_entry = Entry(width=21)
-password_entry.grid(row=3, column = 1)
+password_entry.grid(row=3, column=1)
 
 #Buttons
 generate_password_button = Button(text="Generate Password")
